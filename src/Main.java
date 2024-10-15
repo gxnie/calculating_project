397d96c5239995b0dc576e19e14d0a4d0f32901b
gxnie <wlsdl7520@naver.com>
2024-10-15 13:58:54 +0900

    comment: 레벨1기능 설명 주석 추가

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        while(true){
+        while(true){ // 무한으로 반복
             System.out.print("첫번째 숫자를 입력해주세요 : ");
             int num1 = sc.nextInt();
             System.out.print("두번째 숫자를 입력해주세요 : ");
@@ -31,23 +31,23 @@ public class Main {
                     break;
 
                 case '/':
-                    if (num2 == 0){
+                    if (num2 == 0){ // num2의 값이 0이 입력될 수 없음 -> num2의 값이 0이되면 제어문 종료
                         System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
                         break;
                     }
                     result = num1 / num2;
                     System.out.println("결과 : " + result);
                     break;
+
                 default:
-                    System.out.println("잘 못 입력하였습니다.");
+                    System.out.println("잘 못 입력하였습니다."); // +,-,*,/ 제외한 사칙연산 입력 시
                     break;
             }
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료) : ");
-            String exit = sc.next();
-            if(exit.equals("exit")){
+            String exit = sc.next(); // 입력받을 변수
+            if(exit.equals("exit")){ // exit를 입력한다면, 반복문 종료
                 break;
             }
-            continue;
         }
 
     }
