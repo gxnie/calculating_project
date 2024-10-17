d570410fa1a636f925871b9e1915b27da7b4ed8f
gxnie <wlsdl7520@naver.com>
2024-10-17 12:07:07 +0900

    feat: 분자 또는 분모가 0일때 예외처리 추가

--- src/level2/Calculator.java
+++ src/level2/Calculator.java
@@ -4,14 +4,15 @@ import java.util.LinkedList;
 
 public class Calculator {
     // 연산 결과를 저장하는 컬렉션 타입 필드 선언 및 생성
-    // ArrayList는 배열처럼 일렬로 데이터 저장/조회 - 필요할 때마다 크기 늘어남
-    LinkedList<Integer> result = new LinkedList<>();
+    // LinkedList는 배열처럼 일렬로 데이터 저장/조회 - 필요할 때마다 크기 늘어남
+
+    private final LinkedList<Integer> result = new LinkedList<>();
     int total = 0;
 
     // 연산 기호와 양의 정수를 매개변수로 받아 사칙연산 기능 수행
     // 나눗셈을 할 경우를 대비해 double
     public void calculate(int firstNumber, int secondNumber, char operator) {
-        /* 사칙연산 기능 수행 */
+        // 사칙연산 기능 수행
         switch (operator) { // 사칙연산 기호 기준
             case '+':
                 total = firstNumber + secondNumber;
@@ -26,17 +27,22 @@ public class Calculator {
                 break;
 
             case '/':
-                if (secondNumber == 0) {
-                    // secondNumber의 값이 0이 입력될 수 없음 -> secondNumber의 값이 0이되면 제어문 종료
+                try {
+                    // 0을 입력했을 때, 예외 발생
+                    // ArithmeticException
+                     total = firstNumber / secondNumber;
+                } catch (ArithmeticException e) {
+                    // 0을 입력했을 때 반복문 종료
                     System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
                     break;
-                    // 0으로 했을 시 제어문 종료 + 컬렉션에 값 추가되면 안됨
                 }
+                // 0으로 했을 시 제어문 종료 + 컬렉션에 값 추가되면 안됨
                 total = firstNumber / secondNumber;
                 break;
 
             default:
-                // 사칙연산 기호 4가지를 제외하고 입력했을 경우
+                // 다른 기호를 적었을 때
+                System.out.println("사칙연산(+, -, *, /) 기호를 제대로 입력해주세요.");
                 break;
         }
         this.result.add(total);
@@ -45,14 +51,9 @@ public class Calculator {
         // 값을 추가하는 코드
     }
 
-//    // 연산 결과를 저장하는 컬렉션 타입 필드(=클래스에 포함된 컬렉션 타입 변수)
-//    public void addResult(Integer resultValue) {
-//        // result는 <Integer>의 반환타입을 가지고 있으니, 매개변수도 Integer 반환타입을 갖는다.
-//        // resultValue값을 result에 넣는다.
-//        result.add(resultValue);
-//    }
-
-    // 값을 반환하는 메서드
+    // 결과값을 반환하는 메서드
+    // 객체의 필드 값을 가져오는 역할
+    // 필드(result)는 클래스 내부에 숨겨두고, 외부에 접근 못하게 -> private 설정
     public LinkedList<Integer> getResult(){
         return result;
     }
@@ -64,9 +65,5 @@ public class Calculator {
         // result(컬렉션)에 저장된 값 중 제일 첫번째 값을 삭제
       result.removeFirst();
     }
-
-
-
-
 }
 
